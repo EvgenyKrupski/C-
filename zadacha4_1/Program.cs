44663a3811611682784b585932b256a3dbc203b1
EvgenyKrupski <Krupev87@yandex.ru>
2022-04-11 22:10:11 +0300

    zadacha4_11

--- zadacha4_1/Program.cs
+++ zadacha4_1/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.Write("Введи число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+int numberB = -numberA;
+
+while (numberB <= numberA)
+{
+    Console.WriteLine("Число: " + numberB);
+    numberB+=1;
+}
