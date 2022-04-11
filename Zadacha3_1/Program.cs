7c8d87ab37eddfe49a3a23575cdda6ba548f6592
EvgenyKrupski <Krupev87@yandex.ru>
2022-04-11 21:11:38 +0300

    zadacha3_1

---
+++ Zadacha3_1/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Введите число дня недели: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+if (numberC == 1)
+{
+    Console.Write("Понедельник");
+}
+else
+{
+  if (numberC == 2)
+{
+    Console.Write("Вторник");
+}
+else  
+{
+  if (numberC == 3)
+{
+    Console.Write("Среда");
+}
+else 
+{
+  if (numberC == 4)
+{
+    Console.Write("Четверг");
+}
+else 
+{
+  if (numberC == 5)
+{
+    Console.Write("Пятница");
+}
+else 
+{
+  if (numberC == 6)
+{
+    Console.Write("Суббота");
+}
+else 
+{
+  if (numberC == 7)
+{
+    Console.Write("Воскресенье");
+}}}}}}}
