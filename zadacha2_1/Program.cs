c74dd48ec09e5031a51c5bb97c79e49790793592
EvgenyKrupski <Krupev87@yandex.ru>
2022-04-11 20:54:46 +0300

    zadacha2_1

---
+++ zadacha2_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введи первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введи второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int square = numberA * numberA;
+
+if (square == numberB)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
